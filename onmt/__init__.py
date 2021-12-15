fe0e06617afd7991940adc3fc6fda05d1711f247
Michele Boggia <michele.boggia@getjenny.com>
2021-12-15 15:31:38 +0200

    formatting and small fizes

--- onmt/__init__.py
+++ onmt/__init__.py
@@ -1,20 +1,23 @@
 """ Main entry point of the ONMT library """
 from __future__ import division, print_function
 
-import onmt.inputters
-import onmt.encoders
-import onmt.decoders
-import onmt.models
-import onmt.utils
-import onmt.modules
-from onmt.trainer import Trainer
 import sys
+
+import onmt.decoders
+import onmt.encoders
+import onmt.inputters
+import onmt.models
+import onmt.modules
+import onmt.opts
+import onmt.utils
 import onmt.utils.optimizers
+from onmt.trainer import Trainer
+
 onmt.utils.optimizers.Optim = onmt.utils.optimizers.Optimizer
 sys.modules["onmt.Optim"] = onmt.utils.optimizers
 
 # For Flake
 __all__ = [onmt.inputters, onmt.encoders, onmt.decoders, onmt.models,
-           onmt.utils, onmt.modules, "Trainer"]
+           onmt.utils, onmt.modules, "Trainer", onmt.opts]
 
 __version__ = "0.9.0"
--- translate_multimodel.py
+++ translate_multimodel.py
@@ -1,19 +1,21 @@
-from translate import *
-import argparse, onmt
+import argparse
+import onmt
 
 from ipdb import launch_ipdb_on_exception
 
+import translate
+
 if __name__ == "__main__":
-    parser = ArgumentParser(
-        description='translate.py',
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser = translate.ArgumentParser(
+        description='translate_multimodel.py',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
     #onmt.opts.add_md_help_argument(parser)
     onmt.opts.translate_opts(parser)
     # only this line is extra to normal translate.py
     onmt.opts.translate_multimodel(parser)
 
     opt = parser.parse_args()
-    logger = init_logger(opt.log_file)
+    logger = translate.init_logger(opt.log_file)
     with launch_ipdb_on_exception():
-        main(opt)
-    #main(opt)
+        translate.main(opt)
