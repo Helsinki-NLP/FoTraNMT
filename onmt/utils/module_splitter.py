ab4153e67fe06469fe01c82b6aa3b789a5de7f93
Michele Boggia <michele.boggia@getjenny.com>
2021-12-16 21:57:39 +0200

    modular save - fixing cross-device language pairs

--- onmt/utils/module_splitter.py
+++ onmt/utils/module_splitter.py
@@ -64,10 +64,59 @@ def explode_model(full_ab_model):
     ab_module = OrderedDict()
     gen_modules = list()
 
+    enc_lang_to_ids = full_ab_model["whole_model"].encoder_ids
+    dec_lang_to_ids = full_ab_model["whole_model"].decoder_ids
+    enc_ids_to_lang = {index: lang for lang, index in enc_lang_to_ids.items()}
+    dec_ids_to_lang = {index: lang for lang, index in dec_lang_to_ids.items()}
+
     # extract encoders
     for i, encoder in enumerate(full_ab_model["whole_model"].encoders):
+        lang = enc_ids_to_lang[i]
         enc_model = OrderedDict()
-        enc_model["whole_model"] = encoder
+        enc_model["whole_model"] = OrderedDict(
+            [
+                ("encoder", encoder),
+                (
+                    "encoder_ids",  # TODO: remove if not used in build_bilingual_model (but can be used for a multilingual model)
+                    {lang: i},
+                ),
+                (
+                    "encoder_fams",  # TODO: remove if not used in build_bilingual_model (but can be used for a multilingual model)
+                    {lang: full_ab_model["whole_model"].encoder_fams[lang]},
+                ),
+                (
+                    "encoder_grps",  # TODO: remove if not used in build_bilingual_model (but can be used for a multilingual model)
+                    {lang: full_ab_model["whole_model"].encoder_grps[lang]},
+                ),
+                ("tgt_vocabs", {lang: full_ab_model["whole_model"].tgt_vocabs[lang]}),
+                (
+                    "_modules",
+                    OrderedDict(
+                        [
+                            (
+                                "encoder",
+                                OrderedDict(
+                                    [
+                                        (
+                                            "module",
+                                            full_ab_model["whole_model"]._modules[
+                                                "encoders"
+                                            ][i],
+                                        ),
+                                        (
+                                            "_modules",
+                                            full_ab_model["whole_model"]
+                                            ._modules["encoders"]
+                                            ._modules[str(i)],
+                                        ),
+                                    ]
+                                ),
+                            )
+                        ]
+                    ),
+                ),
+            ]
+        )
         enc_model["model"] = _extract_matching_keys(
             full_ab_model["model"],
             "encoders.{}.".format(i),
@@ -82,8 +131,48 @@ def explode_model(full_ab_model):
 
     # extract decoders
     for i, decoder in enumerate(full_ab_model["whole_model"].decoders):
+        lang = dec_ids_to_lang[i]
         dec_model = OrderedDict()
-        dec_model["whole_model"] = decoder
+        dec_model["whole_model"] = OrderedDict(
+            [
+                ("decoder", decoder),
+                (
+                    "decoder_ids",  # TODO: remove if not used in build_bilingual_model (but can be used for a multilingual model)
+                    {lang: i},
+                ),
+                (
+                    "decoder_types",
+                    {lang: full_ab_model["whole_model"].decoder_types[lang]},
+                ),
+                ("tgt_vocabs", {lang: full_ab_model["whole_model"].tgt_vocabs[lang]}),
+                (
+                    "_modules",
+                    OrderedDict(
+                        [
+                            (
+                                "decoder",
+                                OrderedDict(
+                                    [
+                                        (
+                                            "module",
+                                            full_ab_model["whole_model"]._modules[
+                                                "decoders"
+                                            ][i],
+                                        ),
+                                        (
+                                            "_modules",
+                                            full_ab_model["whole_model"]
+                                            ._modules["decoders"]
+                                            ._modules[str(i)],
+                                        ),
+                                    ]
+                                ),
+                            )
+                        ]
+                    ),
+                ),
+            ]
+        )
         dec_model["model"] = _extract_matching_keys(
             full_ab_model["model"],
             "decoders.{}.".format(i),
@@ -108,7 +197,37 @@ def explode_model(full_ab_model):
     # extract generators
     for i, generator in enumerate(full_ab_model["whole_model"].generators):
         gen_module = OrderedDict()
-        gen_module["whole_model"] = generator
+        gen_module["whole_model"] = OrderedDict(
+            [
+                ("generator", generator),
+                (
+                    "_modules",
+                    OrderedDict(
+                        [
+                            (
+                                "generator",
+                                OrderedDict(
+                                    [
+                                        (
+                                            "module",
+                                            full_ab_model["whole_model"]._modules[
+                                                "generators"
+                                            ][i],
+                                        ),
+                                        (
+                                            "_modules",
+                                            full_ab_model["whole_model"]
+                                            ._modules["generators"]
+                                            ._modules[str(i)],
+                                        ),
+                                    ]
+                                ),
+                            )
+                        ]
+                    ),
+                ),
+            ]
+        )  # using an OrderedDict for consistency with encoders and decoders
         gen_module["model"] = _extract_matching_keys(
             full_ab_model["model"],
             "generators.{}".format(i),
@@ -126,6 +245,7 @@ def explode_model(full_ab_model):
         gen_modules.append(gen_module)
 
     # stuff necessary to build bilingual models combining modules
+    # TODO: remove the stuff that is already stored in the other modules!
     model_frame = {
         "vocab": full_ab_model["vocab"],
         "opt": full_ab_model["opt"],
@@ -168,49 +288,35 @@ def build_bilingual_model(
     )
     output_model["vocab"] = _compose_vocab(model_frame["vocab"], src_lang, tgt_lang)
 
-    output_model["whole_model"].encoders = ModuleList([enc_module["whole_model"]])
-    output_model["whole_model"].decoders = ModuleList([dec_module["whole_model"]])
-    output_model["whole_model"].generators = ModuleList([gen_module["whole_model"]])
+    output_model["whole_model"].encoders = ModuleList(
+        [enc_module["whole_model"]["encoder"]]
+    )
+    output_model["whole_model"].decoders = ModuleList(
+        [dec_module["whole_model"]["decoder"]]
+    )
+    output_model["whole_model"].generators = ModuleList(
+        [gen_module["whole_model"]["generator"]]
+    )
 
     output_model["whole_model"].decoder_ids = {tgt_lang: 0}
     output_model["whole_model"].encoder_ids = {src_lang: 0}
-    output_model["whole_model"].decoder_types = {
-        tgt_lang: model_frame["whole_model"].decoder_types[tgt_lang]
-    }
+    output_model["whole_model"].decoder_types = dec_module["whole_model"][
+        "decoder_types"
+    ]
     output_model["whole_model"].encoder_fams = {src_lang: 0}
     output_model["whole_model"].encoder_grps = {src_lang: 0}
     output_model["whole_model"].tgt_vocabs = {
-        tgt_lang: model_frame["whole_model"].tgt_vocabs[tgt_lang]
+        **enc_module["whole_model"]["tgt_vocabs"],
+        **dec_module["whole_model"]["tgt_vocabs"],
     }
     output_model["whole_model"]._modules["encoders"]._modules = OrderedDict(
-        [
-            (
-                "0",
-                model_frame["whole_model"]
-                ._modules["encoders"]
-                ._modules[str(src_langs_to_id[src_lang])],
-            )
-        ]
+        [("0", enc_module["whole_model"]["_modules"]["encoder"]["_modules"])]
     )
     output_model["whole_model"]._modules["decoders"]._modules = OrderedDict(
-        [
-            (
-                "0",
-                model_frame["whole_model"]
-                ._modules["decoders"]
-                ._modules[str(tgt_langs_to_id[tgt_lang])],
-            )
-        ]
+        [("0", dec_module["whole_model"]["_modules"]["decoder"]["_modules"])]
     )
     output_model["whole_model"]._modules["generators"]._modules = OrderedDict(
-        [
-            (
-                "0",
-                model_frame["whole_model"]
-                ._modules["generators"]
-                ._modules[str(tgt_langs_to_id[tgt_lang])],
-            )
-        ]
+        [("0", gen_module["whole_model"]["_modules"]["generator"]["_modules"])]
     )
 
     return output_model
